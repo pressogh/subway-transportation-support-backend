77d3a2412f44e762a35e7c115c1a5a8d1d547fc7
PRESSO <caff1nepill@gmail.com>
2023-08-24 15:35:32 +0900

    commit migration files

---
+++ prisma/migrations/20230822155658_change_username_to_email/migration.sql
@@ -0,0 +1,20 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `updatedAt` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `username` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropIndex
+DROP INDEX `User_username_key` ON `User`;
+
+-- AlterTable
+ALTER TABLE `User` DROP COLUMN `password`,
+    DROP COLUMN `updatedAt`,
+    DROP COLUMN `username`,
+    ADD COLUMN `email` VARCHAR(191) NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_email_key` ON `User`(`email`);
